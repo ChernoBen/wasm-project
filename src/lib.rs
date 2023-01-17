b12ce8b3093c5b3f3619eec48d4769aeaf4e1289
benjamim.neto <benjamim.neto@azion.com>
2023-01-17 13:41:59 -0300

    mostrar pro malheiro

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use wasm_bindgen::prelude::*;
 use wee_alloc::WeeAlloc;
+use image::{ImageBuffer, Rgba};
 
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
@@ -10,8 +11,20 @@ pub fn hello(text:&str){
     alert(&output_text);
 }
 
-
+//ImageBuffer<Rgba<u8>, Vec<u8>>
 #[wasm_bindgen]
+pub fn img_resize(img_vec: Vec<u8>) -> Result<ImageBuffer<Rgba<u8>, Vec<u8>>,JsValue> {
+        
+    let resized = image::imageops::resize(
+        &img_vec,
+        151,
+        201,
+        image::imageops::FilterType::Lanczos3
+    );
+    //resized.save("mario-ouput.png")?;
+
+    Ok(resized)
+}
 
 #[wasm_bindgen]
 extern {
