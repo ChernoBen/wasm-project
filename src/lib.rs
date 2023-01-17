985bd98114b05d1a89a2d8e6af2c19fea20db02f
benjamim.neto <benjamim.neto@azion.com>
2023-01-17 17:57:28 -0300

    ok so falta funcionar

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 use wee_alloc::WeeAlloc;
-use image::{ImageBuffer, Rgba};
+use image::{io::Reader as ImageReader, flat::Error};
 
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
@@ -11,20 +11,60 @@ pub fn hello(text:&str){
     alert(&output_text);
 }
 
-//ImageBuffer<Rgba<u8>, Vec<u8>>
-#[wasm_bindgen]
-pub fn img_resize(img_vec: Vec<u8>) -> Result<ImageBuffer<Rgba<u8>, Vec<u8>>,JsValue> {
+// //ImageBuffer<Rgba<u8>, Vec<u8>>
+// #[wasm_bindgen]
+// pub fn img_resize(img_vec: Vec<u8>) -> Result<ImageBuffer<Rgba<u8>, Vec<u8>>,JsValue> {
         
-    let resized = image::imageops::resize(
-        &img_vec,
-        151,
-        201,
-        image::imageops::FilterType::Lanczos3
-    );
-    //resized.save("mario-ouput.png")?;
+//     let resized = image::imageops::resize(
+//         &img_vec,
+//         151,
+//         201,
+//         image::imageops::FilterType::Lanczos3
+//     );
+//     //resized.save("mario-ouput.png")?;
 
-    Ok(resized)
+//     Ok(resized)
+// }
+
+
+//use std::io::Cursor;
+//use image::DynamicImage;
+
+//slice: &[u8]
+// #[wasm_bindgen]
+// pub fn img_resize(mut source_img: &[u8]) -> Result<(), JsValue> {
+//   //let img = ImageReader::open("./src/mario.png")?.decode()?;
+//   let img_slice = *(source_img).to_vec();
+//   let img_vec = img_slice.clone();
+//   let resized = image::imageops::resize(
+//     &img_vec,
+//     141,
+//     191,
+//     image::imageops::FilterType::Lanczos3
+//   );
+//   //resized.save("mario-ouput.png")?;
+//   Ok(())
+// }
+
+#[wasm_bindgen]
+pub fn img_resize(path: &str) -> Result<String,JsError> {
+    let img = ImageReader::open(path)?.decode()?;
+    let resized = image::imageops::resize(
+      &img,
+      141,
+      191,
+      image::imageops::FilterType::Lanczos3
+    );
+    let vec_content = resized.into_raw();
+    let slice_content = vec_content.as_slice();
+    let content = String::from_utf8_lossy(slice_content);
+    let response = format!("{}",content);
+    
+    //resized.save("mario-ouput.png")?;
+    alert("entrou!!");
+    Ok(response)
 }
+
 
 #[wasm_bindgen]
 extern {
--- www/index.js
+++ www/index.js
@@ -1,6 +1,12 @@
-import init,{hello} from "../pkg";
+import init,{hello,img_resize} from "../pkg";
 
 init().then(_=>{
-    hello("testandooo!!!");
+    hello("testandooo!!!"); 
     console.log("show show");
+    try{
+        const content = img_resize("./mario.png");
+        console.log(typeof content);
+    }catch(error){
+        console.error(error);
+    }
 });
