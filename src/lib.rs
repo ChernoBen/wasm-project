36e54c28e54e7fb0bfcaac6c4fdcbd6b50da8b19
Benjamim Francisco <bneto@computacao.cua.ufmt.br>
2023-01-18 14:39:40 -0300

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-use try_catch::catch;
 use wasm_bindgen::prelude::*;
 use wee_alloc::WeeAlloc;
-use image::{io::Reader as ImageReader};
-use std::{*, error::Error};
 
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
@@ -13,69 +10,22 @@ pub fn hello(text:&str){
     alert(&output_text);
 }
 
-// //ImageBuffer<Rgba<u8>, Vec<u8>>
-// #[wasm_bindgen]
-// pub fn img_resize(img_vec: Vec<u8>) -> Result<ImageBuffer<Rgba<u8>, Vec<u8>>,JsValue> {
-        
-//     let resized = image::imageops::resize(
-//         &img_vec,
-//         151,
-//         201,
-//         image::imageops::FilterType::Lanczos3
-//     );
-//     //resized.save("mario-ouput.png")?;
-
-//     Ok(resized)
-// }
-
-
-//use std::io::Cursor;
-//use image::DynamicImage;
-
-//slice: &[u8]
-// #[wasm_bindgen]
-// pub fn img_resize(mut source_img: &[u8]) -> Result<(), JsValue> {
-//   //let img = ImageReader::open("./src/mario.png")?.decode()?;
-//   let img_slice = *(source_img).to_vec();
-//   let img_vec = img_slice.clone();
-//   let resized = image::imageops::resize(
-//     &img_vec,
-//     141,
-//     191,
-//     image::imageops::FilterType::Lanczos3
-//   );
-//   //resized.save("mario-ouput.png")?;
-//   Ok(())
-// }
 
 #[wasm_bindgen]
-pub fn img_resize(path: &str) -> Result<String,JsError> {
-    catch! {
-        try {
-            let img = ImageReader::open(path)?.decode()?;
-            let resized = image::imageops::resize(
-              &img,
-              141,
-              191,
-              image::imageops::FilterType::Lanczos3
-            );
-            let vec_content = resized.into_raw();
-            let slice_content = vec_content.as_slice();
-            let content = String::from_utf8_lossy(slice_content);
-            let response = format!("{}",content);
-            
-            //resized.save("mario-ouput.png")?;
-            alert("entrou!!");
-            Ok(response)
-        }
-        catch error: io::Error {
-            println!("Failed to open the file: {}", error);
-            let str_err = error.to_string().as_str();
-            Ok(JsError::new(str_err))
-        }
-    };
-
-    
+pub fn img_resize(source_img:Vec<u8>) -> Result<Vec<u8>,JsError> {
+    let result = image::load_from_memory_with_format(&source_img,image::ImageFormat::Png);
+    let dcd_img = result.unwrap();
+    let resized = image::imageops::resize(
+        &dcd_img,
+        600,
+        400,
+        image::imageops::FilterType::Gaussian
+    );
+    //let _ = resized.save("./gato-output.png");
+    //let test = resized.to_owned().into_vec();
+    let response = resized.into_raw();
+    alert("parece que deu bom");
+    Ok(response)
 }
 
 
--- www/index.js
+++ www/index.js
@@ -4,8 +4,28 @@ init().then(_=>{
     hello("testandooo!!!"); 
     console.log("show show");
     try{
-        const content = img_resize("./mario.png");
-        console.log(typeof content);
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_1280.png", true);
+        xhr.responseType = "arraybuffer";
+        
+        xhr.onload = function (e) {
+          try{
+            var arrayBufferView = new Uint8Array(this.response);
+            var content = img_resize(arrayBufferView);
+            console.log(arrayBufferView);
+            var blob = new Blob([content],{ type: "image/png"});
+            //var urlCreator = URL;
+            var imageUrl = URL.createObjectURL(blob);
+            var img = document.querySelector("#photo");
+            img.src = imageUrl;
+
+          }catch(error){
+            console.log(error.constructor.name);
+          }
+        };
+
+        xhr.send();
+        
     }catch(error){
         if (error){
             console.error(error);
