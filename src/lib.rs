cae44671cfaab954840fcf4ec19a7be481513e35
Benjamim Francisco <bneto@computacao.cua.ufmt.br>
2023-01-19 00:24:54 -0300

    oque e aspect ration???

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
-use wasm_bindgen::prelude::*;
+use photon_rs::PhotonImage;
+use wasm_bindgen::{prelude::*};
+use web_sys::ImageData;
 use wee_alloc::WeeAlloc;
+use image::io::Reader;
+use std::{io::{Cursor}};
+
+extern crate photon_rs;
+
 
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
@@ -10,20 +17,29 @@ pub fn hello(text:&str){
     alert(&output_text);
 }
 
+#[wasm_bindgen]
+//source_img: &[u8]
+pub fn new_resizer(source_img: &[u8],wdth:u32,hgth:u32)->Result<PhotonImage,JsError>{
+    let content = photon_rs::native::open_image_from_bytes(source_img).unwrap();
+    let test = photon_rs::PhotonImage::new(content.get_raw_pixels(), wdth, hgth);
+    Ok(test)
+}
 
 #[wasm_bindgen]
 pub fn img_resize(source_img:Vec<u8>) -> Result<Vec<u8>,JsError> {
-    let result = image::load_from_memory_with_format(&source_img,image::ImageFormat::Png);
-    let dcd_img = result.unwrap();
+    let cursor = Cursor::new(source_img);
+    let reader = Reader::new(cursor).with_guessed_format()
+    .expect("This will never fail using Cursor");
+    let img_reader = reader.decode().expect("Failed to read image");
     let resized = image::imageops::resize(
-        &dcd_img,
+        &img_reader,
         600,
         400,
         image::imageops::FilterType::Gaussian
     );
-    //let _ = resized.save("./gato-output.png");
-    //let test = resized.to_owned().into_vec();
     let response = resized.into_raw();
+    
+    //let unwrapped_vec = img_reader.into_rgb8().into_raw();
     alert("parece que deu bom");
     Ok(response)
 }
--- www/index.js
+++ www/index.js
@@ -1,37 +1,60 @@
-import init,{hello,img_resize} from "../pkg";
+import init,{hello,img_resize,new_resizer} from "../pkg";
 
-init().then(_=>{
+
+init().then(async _=>{
     hello("testandooo!!!"); 
     console.log("show show");
-    try{
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_1280.png", true);
-        xhr.responseType = "arraybuffer";
-        
-        xhr.onload = function (e) {
-          try{
-            var arrayBufferView = new Uint8Array(this.response);
-            var content = img_resize(arrayBufferView);
-            console.log(arrayBufferView);
-            var blob = new Blob([content],{ type: "image/png"});
-            //var urlCreator = URL;
-            var imageUrl = URL.createObjectURL(blob);
-            var img = document.querySelector("#photo");
-            img.src = imageUrl;
 
-          }catch(error){
-            console.log(error.constructor.name);
-          }
-        };
+    const img = await fetch("https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_1280.png");
+	const blolb = await img.blob();
+	const array_buff = await blolb.arrayBuffer();
+	const data = new Uint8Array(array_buff);
+    //console.log(typeof data);
+    //const res = new_resizer(data);
+    const width = 1281;
+    const proportion = 1.5041128085;
+    const default_h = 851;
+    const aspectRatio = width/default_h
+    const height = default_h/aspectRatio;
 
-        xhr.send();
+    const res = new_resizer(data,width,height);
+    console.log(res.get_base64());
+    const t2 = Uint8Array.from(window.atob(res.get_base64().replace(/^data[^,]+,/,'')), v => v.charCodeAt(0));
+    var blb = new Blob([t2],{ type: "image/png"});
+    var imageUrl = URL.createObjectURL(blb);
+    var page = document.querySelector("#photo");
+    page.src = imageUrl;
+
+
+    // try{
+    //     var xhr = new XMLHttpRequest();
+    //     xhr.open("GET", "https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_1280.png", true);
+    //     xhr.responseType = "arraybuffer";
+    //     xhr.onload = function (e) {
+    //       try{
+    //         var arrayBufferView = new Uint8Array(this.response);
+    //         var content = img_resize(arrayBufferView);
+    //         content = new Uint8Array(content);
+    //         var content2 = new_resizer(arrayBufferView.slice());
+    //         var img_data = content2.get_image_data().data;
+    //         console.log(content2.constructor.name);
+    //         console.log(content2.get_image_data().data);
+    //         var blob = new Blob([img_data],{ type: "image/png"});
+    //         var imageUrl = URL.createObjectURL(blob);
+    //         var img = document.querySelector("#photo");
+    //         img.src = imageUrl;
+    //       }catch(error){
+    //         console.error(error);
+    //       }
+    //     };
+    //     xhr.send();
+
+    // }catch(error){
+    //     if (error){
+    //         console.error(error);
+    //     }else{
+    //         console.log("kd o erro???")
+    //     }
         
-    }catch(error){
-        if (error){
-            console.error(error);
-        }else{
-            console.log("kd o erro???")
-        }
-        
-    }
+    // }
 });
