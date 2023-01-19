636d9711cca9ff28d70965aaf3d304208972cd33
Benjamim Francisco <bneto@computacao.cua.ufmt.br>
2023-01-19 11:41:53 -0300

    haha consegui caralhoooo

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 use photon_rs::PhotonImage;
 use wasm_bindgen::{prelude::*};
-use web_sys::ImageData;
 use wee_alloc::WeeAlloc;
-use image::io::Reader;
-use std::{io::{Cursor}};
 
 extern crate photon_rs;
 
@@ -20,30 +17,10 @@ pub fn hello(text:&str){
 #[wasm_bindgen]
 //source_img: &[u8]
 pub fn new_resizer(source_img: &[u8],wdth:u32,hgth:u32)->Result<PhotonImage,JsError>{
-    let content = photon_rs::native::open_image_from_bytes(source_img).unwrap();
-    let test = photon_rs::PhotonImage::new(content.get_raw_pixels(), wdth, hgth);
+    let  content = photon_rs::native::open_image_from_bytes(source_img).unwrap();
+    let test = photon_rs::transform::resize(&content, wdth,hgth, photon_rs::transform::SamplingFilter::Gaussian);
     Ok(test)
 }
-
-#[wasm_bindgen]
-pub fn img_resize(source_img:Vec<u8>) -> Result<Vec<u8>,JsError> {
-    let cursor = Cursor::new(source_img);
-    let reader = Reader::new(cursor).with_guessed_format()
-    .expect("This will never fail using Cursor");
-    let img_reader = reader.decode().expect("Failed to read image");
-    let resized = image::imageops::resize(
-        &img_reader,
-        600,
-        400,
-        image::imageops::FilterType::Gaussian
-    );
-    let response = resized.into_raw();
-    
-    //let unwrapped_vec = img_reader.into_rgb8().into_raw();
-    alert("parece que deu bom");
-    Ok(response)
-}
-
 
 #[wasm_bindgen]
 extern {
--- www/index.js
+++ www/index.js
@@ -4,19 +4,18 @@ import init,{hello,img_resize,new_resizer} from "../pkg";
 init().then(async _=>{
     hello("testandooo!!!"); 
     console.log("show show");
-
+    //var cat_size = document.getElementById("size").value;
     const img = await fetch("https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_1280.png");
 	const blolb = await img.blob();
 	const array_buff = await blolb.arrayBuffer();
 	const data = new Uint8Array(array_buff);
     //console.log(typeof data);
     //const res = new_resizer(data);
-    const width = 1281;
-    const proportion = 1.5041128085;
-    const default_h = 851;
+    const width = 300;
+    //const proportion = 1.5041128085;
+    const default_h = 250;
     const aspectRatio = width/default_h
-    const height = default_h/aspectRatio;
-
+    const height = default_h///aspectRatio;
     const res = new_resizer(data,width,height);
     console.log(res.get_base64());
     const t2 = Uint8Array.from(window.atob(res.get_base64().replace(/^data[^,]+,/,'')), v => v.charCodeAt(0));
